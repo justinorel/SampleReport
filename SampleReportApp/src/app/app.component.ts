42e0efd0efbde973347f928a388af0cedd984497
Justin Orel <Justin.Orel@bentley.com>
2017-12-05 10:46:55 -0600

    set view encapsulation to none

--- SampleReportApp/src/app/app.component.ts
+++ SampleReportApp/src/app/app.component.ts
@@ -1,11 +1,12 @@
-﻿import { Component, OnInit } from '@angular/core';
+﻿import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 import { Global } from './global';
 
 @Component({
   selector: 'app',
   moduleId: module.id,
   templateUrl: 'app.component.html',
-  styleUrls: ['app.component.css', "..\..\node_modules\@progress\kendo-theme-default\dist\all.css"]
+  styleUrls: ['app.component.css', "..\..\node_modules\@progress\kendo-theme-default\dist\all.css"],
+  encapsulation: ViewEncapsulation.None
 })
 export class AppComponent implements OnInit{
     private startDate: Date;
